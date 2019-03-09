5bd94215b2a5d81c0540d2a9681587142a04a845
Gabriel Vlasek <gvlasek@bluewin.ch>
2019-03-09 11:13:13 +0100

    Add default route

--- server.go
+++ server.go
@@ -12,13 +12,13 @@ type Model struct {
 	Name string
 }
 
-func market(w http.ResponseWriter, r *http.Request, ps httprouter.Params){
-	template, _ := template.ParseFiles("market.gtpl");
-	template.Execute(w, Model{});
+func market(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	template, _ := template.ParseFiles("market.gtpl")
+	template.Execute(w, Model{})
 }
 
-func api(w http.ResponseWriter, r *http.Request, ps httprouter.Params){
-	response, _  := http.Get("https://api.coinmarketcap.com/v1/ticker/?limit=10");
+func api(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	response, _ := http.Get("https://api.coinmarketcap.com/v1/ticker/?limit=10")
 	io.Copy(w, response.Body)
 }
 
@@ -27,5 +27,6 @@ func main() {
 	router := httprouter.New()
 	router.GET("/market", market)
 	router.GET("/api", api)
+	router.GET("/", market)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
